71c7179fcdd2f4c07575f2b51d228da017f6a8d6
Manzi,Eddie <eddie_manzi@vanguard.com>
2023-06-08 15:46:58 +0000

    Challenge 4

--- azure.tf
+++ azure.tf
@@ -38,3 +38,35 @@ resource "azurerm_storage_container" "storageContainer2" {
   storage_account_name  = azurerm_storage_account.team2storage.name
   container_access_type = "blob"
 }
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "vault" {
+  name = "team2vault${random_string.suffix.result}"
+  location = azurerm_resource_group.team2_rg.location
+  resource_group_name = azurerm_resource_group.team2_rg.name
+  sku_name = "standard"
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get", "Set"
+    ]
+  }
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+}
+
+resource "azurerm_key_vault_secret" "hack" {
+  name         = "mysecret"
+  value        = random_password.password.result
+  key_vault_id = azurerm_key_vault.vault.id
+}
